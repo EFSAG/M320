7c4568702c9c87fe790248f15c9580fe11c6cfd9
EFSAG <125400685+EFSAG@users.noreply.github.com>
2023-10-29 12:12:50 +0100

    Add files via upload

---
+++ wasserfass_EfeBoraSaglam/wasserfass_EfeBoraSaglam/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace wasserfass_EfeBoraSaglam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Wasserfass wasserfass1 = new Wasserfass();
+            do
+            {
+                wasserfass1.first();
+                Console.WriteLine("____________________________________________________________________________________________________\n\n");
+                wasserfass1.infos();
+                Console.WriteLine("Was möchtest du machen?\n\n befüllen\t entnehmen\t entleeren");
+                switch (Console.ReadLine())
+                {
+                    case "befüllen":
+                        wasserfass1.befullen();
+                            break;
+                    case "entnehmen":
+                        wasserfass1.entnehmen();
+                            break;
+                    case "entleeren":
+                        wasserfass1.entleeren();
+                        break;
+                    default:
+                        Console.WriteLine("Gib was richtiges ein");
+                        break;
+                }
+                Console.WriteLine("Wollen Sie weiter machen?\n dann schreibe JA");
+            } while (Console.ReadLine().ToUpper() == "JA");
+            Console.WriteLine("Tschüss");
+
+
+
+
+        }
+    }
+}
---
+++ wasserfass_EfeBoraSaglam/wasserfass_EfeBoraSaglam/wasserfass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wasserfass_EfeBoraSaglam
+{
+    internal class Wasserfass
+    {
+        private float MinimalePegelinLiter = 0;
+        private float MaximalePegelinLiter = 500;
+        public float FüllstandinLiter = 222;
+        public float KapazitatinLiter = 505;
+        public string Bauform = "Wurfel";
+        public string Farbe = "Weiss";
+        public string Baumaterial = "Plastik";
+        public string Standort = "St.Gallen";
+
+        public void befullen()
+        {
+
+            double menge;
+            Console.Write("Geben sie den Liter betrag den sie ins Wasserfass befüllen möchten ein: ");
+            menge = float.Parse(Console.ReadLine());
+            if (menge > 278)
+            {
+                Console.WriteLine("Im Wasserfass gibt es leider nicht so viel Platz.");
+            }
+            else
+            {
+                Console.WriteLine("Dein Füllstand ist jetzt: {0}", menge + FüllstandinLiter);
+            }
+        }
+
+        public void entnehmen()
+        {
+            double menge;
+            Console.Write("Geben sie den Liter betrag den sie ins Wasserfass entehmen möchten ein: ");
+            menge = float.Parse(Console.ReadLine());
+            if (menge > 222)
+            {
+                Console.WriteLine("Im Wasserfass gibt es nicht so viel Water");
+            }
+            else
+            {
+                Console.WriteLine("Dein Füllstand ist jetzt: {0}", menge + FüllstandinLiter);
+            }
+        }
+        public void entleeren()
+        {
+            Console.WriteLine("Du hast jetzt {0} Liter Wasser in Wasserfass", FüllstandinLiter - FüllstandinLiter);
+        }
+        public void infos()
+        {
+            Console.WriteLine("Bauform: {0}\n\nFarbe: {1}\n\nBaumaterial: {2}\n\nStandort: {3}\n\n", Bauform, Farbe, Baumaterial, Standort);
+        }
+        public void first()
+        {
+            Console.WriteLine("{0} ist dein minimale Pegel.\n {1} ist dein max Pegel.\n Dein Füllstand ist {2} Liter\n\n", MinimalePegelinLiter, MaximalePegelinLiter, FüllstandinLiter);
+        }
+    }
+}
